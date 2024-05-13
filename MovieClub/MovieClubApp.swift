ab49238d21afeafd2b3b270d771bb293df8e35d3
m-lair <mlair@heidelberg.edu>
2024-05-12 21:03:29 -0500

    Initial Commit

---
+++ MovieClub/MovieClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieClubApp.swift
+//  MovieClub
+//
+//  Created by Marcus Lair on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
