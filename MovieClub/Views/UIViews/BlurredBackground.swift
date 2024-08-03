392dd6be801498abea2dad8a7a4d1b837aa5fa52
m-lair <mlair@heidelberg.edu>
2024-08-03 18:41:08 -0500

    Changed background color of homepage to match figma changed tabView to match figma created blurred background view for club detail view

---
+++ MovieClub/Views/UIViews/BlurredBackground.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  MovieClub
+//
+//  Created by Marcus Lair on 8/1/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
