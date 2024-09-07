6bea279f4ee3d2df4fe5fa3c93d713c141094696
m-lair <mlair@heidelberg.edu>
2024-09-07 14:58:53 -0500

    cloud function now successfully rotates movies in prod on 24hr schedule

--- functions/index.js
+++ functions/index.js
@@ -7,78 +7,105 @@ if (!admin.apps.length) {
     admin.initializeApp();
 }
 
+// Export the scheduled function for deployment
 exports.rotateMovie = functions.pubsub.schedule('every 24 hours').onRun(async () => {
     console.log('Rotating movie...');
     await rotateMovieLogic();
 });
-
-// Export the scheduled function for deployment
-
 async function rotateMovieLogic() {
     console.log('Rotating movie logic...');
     const db = admin.firestore();
+    console.log('after getting db' + db);
     const apiEndpoint = `http://www.omdbapi.com/?apikey=${functions.config().omdbapi.key}&r=json`;
-    const currentTimestamp = admin.firestore.Timestamp.now();
+    console.log('apiEndpoint: ' + apiEndpoint);
+
+    
+    
     try {
-      const snapshot = await db.collectionGroup("movieclubs")
+      console.log('getting snapshot');
+      /*const snapshot = await db.collectionGroup("movieclubs")
         .where("movieEndDate", "<", currentTimestamp)
-        .get();
-  
+        .get();*/
+      const snapshot = await db.collectionGroup("movieclubs").get();
+      console.log('snapshot COUNT: ' + snapshot.docs.length);
       const promises = snapshot.docs.map(async (doc) => {
         console.log(`Rotating movie for club ${doc.id}`);
             // if we are here then we know we have a club 
             // that has a movie that needs to be rotated
+            const currentTimestamp = new Date();
 
             // get movieclub from doc
-
+            console.log('currentTimestamp: ' + currentTimestamp);
+            console.log('movieEndDate: ' + doc.data().movieEndDate);
+            if(doc.data().movieEndDate > currentTimestamp){
+                console.log('movieEndDate > currentTimestamp');
+            }
             const movieclubDoc =  await doc.ref.get();
 
+            const clubInterval =  movieclubDoc.data().timeInterval;
+            const futureDate = new Date(currentTimestamp.setDate(currentTimestamp.getDate() + clubInterval * 7));
             if (!movieclubDoc.exists) {
                 throw new Error(`Movieclub with ID ${doc.id} does not exist.`);
             }
-            const movieclubData = movieclubDoc.data();
             const clubID = doc.id;
             // get the next user in line by ordering the members by dateAdded and limiting to 1
             // this will be the next user to have their movie selected
             const nextUp = await movieclubDoc.ref.collection("members").orderBy("dateAdded", "asc").limit(1).get();
             // get user from doc
+            if (nextUp.docs.length === 0) {
+                throw new Error(`No users in line for club ${clubID}`);
+            }
             const userID = nextUp.docs[0].id;
-            const userRef = db.collection("users").doc(userID);
-            console.log(`Next user in line is ${userID}`);
-            const userDoc = await userRef.get();
+            const userDoc = await db.collection("users").doc(userID).get();
             if (!userDoc.exists) {
                 throw new Error(`User with ID ${userID} does not exist.`);
             }
 
             const userData = userDoc.data();
-            console.log(clubID);
-            const membershipRef = await userDoc.ref.collection("memberships").doc(clubID).get();
 
+            console.log('userData: ' + userData.name);
+            console.log('userID: ' + userID);
+            
+            membershipRef = await userDoc.ref.collection("memberships").doc(clubID).get();
+            console.log('membershipRef: ' + membershipRef.data());
+         
             if (!membershipRef.exists) {
                 throw new Error(`Membership with ID ${clubID} does not exist.`);
             }
             const membershipData = membershipRef.data();
+            if(membershipData.queue.length === 0){
+                throw new Error(`No movies in queue for user ${userData.name}`);
+            }
             const movie = membershipData.queue[0];
 
-            const response = await fetch(`${apiEndpoint}&t=${movie.title}`);
+            console.log('movie: ' + movie.title);
+            const response = await fetch(`${apiEndpoint}&t=${movie.title}`, { method: 'GET' });
+            console.log('response: ' + response);
             if (!response.ok) {
                 throw new Error(`Failed to fetch data for movie ${movie.title}`);
             }
 
             const data = await response.json();
+            if (!data) {
+                throw new Error(`No data found for movie ${movie.title}`); 
+            }
             const newMovieData = {
-                title: data.Title,
-                director: data.Director,
-                plot: data.Plot,
-                author: userData.name,
-                authorID: userData.id,
-                authorAvi: userData.image,
+                title: data.Title ? data.Title : 'No Title',
+                director: data.Director ? data.Director : 'No Director',
+                plot: data.Plot ? data.Plot : 'No Plot',
+                author: userData.name ? userData.name : 'no-name',
+                authorID: userData.id ? userData.id : 'no-id',
+                authorAvi: userData.image ? userData.image : 'no-image',
                 created: currentTimestamp,
-                endDate: currentTimestamp + (1000 * 60 * 60 * 24 * 7)
-            };     
+                endDate: futureDate,
+            };   
+
+            console.log('newMovieData: ' + newMovieData);
+
             db.collection("movieclubs").doc(clubID).collection("movies").doc().set(newMovieData);     
             try {
-            nextUp.docs[0].ref.update({ dateAdded: admin.firestore.Timestamp.now() });
+            nextUp.docs[0].ref.update({ dateAdded: currentTimestamp });
+            movieclubDoc.ref.update({ movieEndDate: futureDate });
             } catch (error) {
                 console.error(`Error resetting dateAdded for membership with ID ${membershipRef.id}:`, error);
             }
