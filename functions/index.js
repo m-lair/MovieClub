0ded00e3c60ef3f6e0e7650e5b712d89a24e653d
Nathan Caldwell <NathanCCaldwell@gmail.com>
2024-09-20 18:06:23 -0400

    update package-lock

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 require('module-alias/register');
+
+const config = require('./config')
 const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
   admin.initializeApp();
 }
 
-const config = require('./config')
+
 const movies = require('./firestore/movieClubs/movies/movieFunctions')
 const movie_clubs = require('./firestore/movieClubs/movieClubFunctions')
 const users = require('./firestore/users/userFunctions')
