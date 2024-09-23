6ea9e0cae531553a1497155a0f944bd1ae1863a7
Nathan Caldwell <NathanCCaldwell@gmail.com>
2024-09-23 04:06:32 -0400

    use local emulator for createUser instead of mocking functions

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -4,3 +4,4 @@ require("module-alias/register");
 const { db } = require('firestore');
   
 db.settings({ host: 'localhost:8080', ssl: false });
+process.env.FIREBASE_AUTH_EMULATOR_HOST = "127.0.0.1:9099"
